58db8c914bddb33219698b790489dde4c411366a
Parthiban-selvam-Dhanam <145418656+Parthiban-selvam-Dhanam@users.noreply.github.com>
2023-12-17 12:22:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function foo(){
+    var res = document.getElementById("Firstname").value;
+    console.log("Firstname =",res);
+    var res1 = document.getElementById("MiddleName").value;
+    console.log("Middlename=",res1);
+    var res2 = document.getElementById("LastName").value;
+    console.log("Lastname=",res2);
+    var res3 = document.getElementById("email").value;
+    console.log("Email =",res3);
+
+    }
+    
